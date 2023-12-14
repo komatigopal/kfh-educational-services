dda9e464b8f1fc8bbcbf51cc89a2f33382e25f69
komatigopal <komati.gopal@gmail.com>
2023-12-14 21:59:14 +0530

    code modified

--- kfh-courses-service/src/main/java/com/kfh/education/courses/controller/CourseController.java
+++ kfh-courses-service/src/main/java/com/kfh/education/courses/controller/CourseController.java
@@ -1,6 +1,5 @@
 package com.kfh.education.courses.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,18 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kfh.education.courses.dto.CourseDto;
-import com.kfh.education.courses.entity.CourseEntity;
 import com.kfh.education.courses.service.CourseService;
 
 import io.swagger.annotations.ApiImplicitParam;
